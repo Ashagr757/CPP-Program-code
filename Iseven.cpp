c970044ce89c026b3da6a7cfb0e1b0c6a93c689f
Ashagr757 <97276081+Ashagr757@users.noreply.github.com>
2026-06-28 22:13:31 +0530

    Add files via upload

---
+++ Iseven.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    if(n%2==0){
+        cout<<"the number you have enter is even number"<<endl;
+    }
+    else{
+        cout<<"the number you have enter is not a even number"<<endl;
+    }
+    return 0;
+}
